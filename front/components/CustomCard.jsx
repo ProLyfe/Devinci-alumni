06cd1d5716805ba4f9264d5e200309ebe4993ba3
Naoufel <naoufel.maazouzi@live.fr>
2023-07-19 22:39:41 +0200

    update

--- front/components/CustomCard.jsx
+++ front/components/CustomCard.jsx
@@ -3,44 +3,64 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
 import { Button, CardActionArea, CardActions } from '@mui/material';
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
 
-export default function MultiActionAreaCard({ userData}) {
+export default function MultiActionAreaCard({ userData, setActiveStep, userSelected, setUserSelected }) {
     const MySwal = withReactContent(Swal)
+    const isSelected = userSelected === userData.id
 
     const handleClick = () => {
         MySwal.fire({
-            title: <strong>Parfait !</strong>,
-            html: <i>Vous avez choisi votre alumni !</i>,
+            title: <strong>Vous avez choisi votre parrain !</strong>,
+            html: <i>Un email lui a été envoyé !</i>,
             icon: 'success'
+          }).then(() => {
+            setUserSelected(userData.id)
+            setActiveStep(3)
           })
     }
 
   return (
-    <Card>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image={userData.image}
-          alt="green iguana"
-        />
-        <CardContent >
-          <Typography gutterBottom variant="h5" component="div" textAlign={'center'}>
-            {userData.firstname} {userData.lastname}
-          </Typography>
-          <Typography variant="body2" color="text.secondary" textAlign={'center'}>
-            {userData.work}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary" style={{margin: '0 auto', display: "flex"}} onClick={handleClick}>
-        Choisir
-        </Button>
-      </CardActions>
-    </Card>
+    <Paper elevation={isSelected ? 15 : 0}>
+      <Card>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image={userData.image}
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div" textAlign={'center'}>
+              {userData.firstname} {userData.lastname}
+            </Typography>
+            <Typography variant="body2" color="text.secondary" textAlign={'center'}>
+              {userData.work}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          {isSelected ? <Button
+            size="small"
+            color="primary"
+            style={{margin: '0 auto', display: "flex", color: "green"}}
+            disabled={true}
+            >
+              Sélectionné
+            </Button> : <Button
+            size="small"
+            color="primary"
+            style={{margin: '0 auto', display: "flex"}}
+            onClick={handleClick}
+            variant='outlined'
+            >
+          Choisir
+          </Button>}
+        </CardActions>
+      </Card>
+    </Paper>
   );
 }
--- front/components/ListOfCard.jsx
+++ front/components/ListOfCard.jsx
@@ -8,30 +8,33 @@ import CustomCard from './CustomCard';
 const allUsers = [
     {
         firstname: 'Benjamin',
-        lastname: 'Huppé',
+        lastname: 'Hup',
         numberOfPeople: 3,
         description: 'Salut ! Je suis développeur Full-Stack chez Crédit Agricole.',
         work: 'Développeur Full-stack',
         image: 'alumni-1.jpg',
-        degree: 'IWM' 
-    },
-    {
-        firstname: 'Rachelle',
-        lastname: 'Brisebois',
-        numberOfPeople: 2,
-        description: 'Salut ! Je suis développeur Front chez Crédit Foncia.',
-        work: 'Développeur Front',
-        image: 'alumni-2.jpg',
-        degree: 'IWM' 
+        degree: 'IWM',
+        id: 2
     },
     {
         firstname: 'Xavier',
         lastname: 'Montiny',
         numberOfPeople: 2,
+        description: 'Salut ! Je suis développeur Front chez Crédit Foncia.',
+        work: 'Développeur Front',
+        image: 'alumni-2.jpg',
+        degree: 'IWM',
+        id: 3
+    },
+    {
+        firstname: 'Rachelle',
+        lastname: 'bois',
+        numberOfPeople: 2,
         description: 'Salut ! Je suis chef de projet chez société générale.',
         work: 'Chef de projet',
         image: 'alumni-3.jpg',
-        degree: 'MTD' 
+        degree: 'MTD',
+        id: 4 
     },
     {
         firstname: 'Marcel',
@@ -40,20 +43,31 @@ const allUsers = [
         description: 'Salut ! Je suis chef de projet chez SFR.',
         work: 'Chef de projet',
         image: 'alumni-4.jpg',
-        degree: 'MTD' 
+        degree: 'MTD',
+        id: 5
     },
 ]
 
-export default function ResponsiveGrid({ userData }) {
+export default function ResponsiveGrid({ userData, setActiveStep, userSelected, setUserSelected }) {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid sx={{ maxWidth: 845 }} container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
         <Grid item xs={2} sm={4} md={4}>
-            <CustomCard userData={userData}/>
+            <CustomCard
+              userData={userData}
+              setActiveStep={setActiveStep}
+              userSelected={userSelected}
+              setUserSelected={setUserSelected}
+            />
         </Grid>
         {allUsers.map((userData, index) => (
           <Grid item xs={2} sm={4} md={4} key={index}>
-            <CustomCard userData={userData}/>
+            <CustomCard
+              userData={userData}
+              setActiveStep={setActiveStep}
+              userSelected={userSelected}
+              setUserSelected={setUserSelected}
+            />
           </Grid>
         ))}
       </Grid>
